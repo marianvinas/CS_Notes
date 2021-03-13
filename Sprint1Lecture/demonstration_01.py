76a14065e3f519c85790dcf7d91c51d48900370c
MarianVinas <marian-vinas@lambdastudents.com>
2021-03-13 00:23:54 -0800

    initial commit

---
+++ Sprint1Lecture/demonstration_01.py
@@ -0,0 +1,15 @@
+"""
+Challenge #1:
+Create a function that takes two numbers as arguments and return their sum.
+Examples:
+- addition(3, 2) -> 5
+- addition(-3, -6) -> -9
+- addition(7,3) -> 10
+"""
+
+def addition(a,b):
+    s = a + b
+    return sum
+
+#printing/testing
+print(addition(3,2))
